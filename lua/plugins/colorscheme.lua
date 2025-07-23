ab7743bebfff4cee4bd9a373bbd1540bf672b36d
Travis Kinney <travis@traviskinney.co>
2025-07-23 03:00:47 -0700

    Add Rose Pine color scheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  "rose-pine/neovim",
+  name = "rose-pine",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("rose-pine").setup({
+      styles = {
+        bold = true,
+        italic = true,
+        transparency = true,
+      },
+    }, vim.cmd([[colorscheme rose-pine]]))
+  end,
+}
