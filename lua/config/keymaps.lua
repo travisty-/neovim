70eb14774105a144f332b8b851cfe72e44583faa
Travis Kinney <travis@traviskinney.co>
2026-05-15 00:35:05 -0700

    Add custom keymap to exit insert mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 --
 -- Add any additional keymaps here:
+
+local map = vim.keymap.set
+
+map("i", "jj", "<Esc>", { desc = "Exit Insert Mode" })
