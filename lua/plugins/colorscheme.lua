244ae74e969ac76a48fcc6875ab0d3a54c6447a7
Travis Kinney <travis@traviskinney.co>
2026-05-15 00:45:39 -0700

    Add Everforest color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,32 @@
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("rose-pine").setup({
-      styles = {
-        bold = true,
-        italic = true,
-        transparency = true,
-      },
-    }, vim.cmd([[colorscheme rose-pine]]))
-  end,
+  {
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.everforest_background = "hard"
+      vim.g.everforest_better_performance = 1
+      vim.g.everforest_diagnostic_virtual_text = "colored"
+      vim.g.everforest_dim_inactive_windows = 1
+      vim.g.everforest_enable_italic = 1
+      vim.g.everforest_float_style = "dim"
+      vim.g.everforest_show_eob = 0
+      vim.g.everforest_transparent_background = 2
+      vim.cmd([[colorscheme everforest]])
+    end,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = true,
+    config = function()
+      require("rose-pine").setup({
+        styles = {
+          bold = true,
+          italic = true,
+          transparency = true,
+        },
+      })
+    end,
+  },
 }
