f6578dd5f2a3cb721a7a6911d6d4238be336deae
Travis Kinney <travis@traviskinney.co>
2026-05-15 11:22:09 -0700

    Add autocmd to change directories on startup

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,3 +5,17 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults).
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = vim.api.nvim_create_augroup("config.autocmds.cd", { clear = true }),
+  callback = function()
+    if vim.fn.argc() == 1 then
+      local arg = vim.fn.argv(0) --[[@as string]]
+      if vim.fn.isdirectory(arg) == 1 then
+        vim.schedule(function()
+          vim.cmd.cd(vim.fn.fnameescape(arg))
+        end)
+      end
+    end
+  end,
+})
