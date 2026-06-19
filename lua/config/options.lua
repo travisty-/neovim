71aaa964ae594aef4e96e1653c771f91299381d0
Travis Kinney <travis@traviskinney.co>
2026-06-19 04:13:14 -0700

    Add transparent color theme (everforest.nvim)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 --
 -- Add any additional options here:
+
+-- Prevents floating windows from visually blending into the main buffer.
+-- Only required when the color scheme's float mode is set to "blend".
+vim.o.winborder = "rounded"
