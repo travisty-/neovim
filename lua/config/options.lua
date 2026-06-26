38876c64ca65f2fd2b182693d0db76532e39d878
Travis Kinney <travis@traviskinney.co>
2026-06-26 16:33:50 -0700

    Disable smart indent to keep comments indented (options.lua)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,10 @@
 --
 -- Add any additional options here:
 
+-- Disable legacy 'smartindent' rule "When you type # as the first character
+-- of a line, the indentation is deleted and the # is forced to column 0."
+vim.o.smartindent = false
+
 -- Prevents floating windows from visually blending into the main buffer.
 -- Only required when the color scheme's float mode is set to "blend".
 vim.o.winborder = "rounded"
