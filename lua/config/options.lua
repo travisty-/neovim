8991acd56b723c37dc58d79f874cd25ebefe3133
Travis Kinney <travis@traviskinney.co>
2026-07-02 22:34:46 -0700

    Patch the default cursor styles (options.lua)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,12 @@
 --
 -- Add any additional options here:
 
+-- stylua: ignore
+-- Patch the default cursor styles for each mode.
+vim.o.guicursor = vim.o.guicursor
+  :gsub("i%-ci%-ve:ver25", "%0-blinkon500-blinkoff500")
+  :gsub("t:block", "t:ver25")
+
 -- Disable legacy 'smartindent' rule "When you type # as the first character
 -- of a line, the indentation is deleted and the # is forced to column 0."
 vim.o.smartindent = false
