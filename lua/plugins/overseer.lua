f2b91250b4c08cad558daef85143117122ed75f3
Travis Kinney <travis@traviskinney.co>
2026-07-17 03:30:33 -0700

    Add Obsidian plugin (obsidian.nvim)

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -1,8 +1,22 @@
 return {
-  "folke/which-key.nvim",
-  opts = {
-    spec = {
-      { "<leader>o", group = "overseer", icon = { icon = "\u{ea70}", color = "red" } },
+  {
+    "stevearc/overseer.nvim",
+    -- stylua: ignore
+    keys = {
+      { "<leader>oo", false },
+      { "<leader>ot", false },
+      { "<leader>ow", false },
+      { "<leader>ta", "<cmd>OverseerTaskAction<cr>", desc = "Task Action" },
+      { "<leader>tl", "<cmd>OverseerToggle!<cr>",    desc = "Task List" },
+      { "<leader>tt", "<cmd>OverseerRun<cr>",        desc = "Run Task" },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>t", group = "overseer", icon = { icon = "\u{ea70}", color = "red" } },
+      },
     },
   },
 }
